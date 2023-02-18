1d647c6fe3bfce46e9ddebfbc09b71d144d5254d
Ichabodi <106030802+Ichabodi@users.noreply.github.com>
2023-02-18 18:09:23 +0200

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-data = pd.read_csv('Salary.csv')
-print(data.info)
-print(data.shape)
-print(data.set_option('display.max_columns',23))
-print(data.set_option('display.max_rows',23))
-figure=pd.read_csv('data/Salary.cvs')
-print(figure)
